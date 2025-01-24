e3474b03397fc8c32672e3958601904d1ce2997d
jpwol <jpwolfe1981@gmail.com>
2025-01-24 15:40:16 -0500

    Added git integration

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gg", "<cmd>Gitsigns<CR>", { desc = "Open Git menu" })
+			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Open the preview menu" })
+			vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<CR>", { desc = "View next git hunk" })
+			vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<CR>", { desc = "View previous git hunk" })
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+}
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "filename" },
+				lualine_x = { "diagnostics", "encoding", "fileformat", "filetype" },
+			},
+		})
+	end,
+}
