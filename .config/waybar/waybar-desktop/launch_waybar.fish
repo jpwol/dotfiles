1b60133340afd9390d1f35c34ec69799403a3991
Josh <jpwolfe1981@gmail.com>
2025-01-30 13:07:44 -0500

    Desktop rebasing

--- .config/waybar/waybar-desktop/launch_waybar.fish
+++ .config/waybar/waybar-desktop/launch_waybar.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/zsh
+
 # kill active instances of waybar
-killall waybar
+kill $(pidof waybar)
 
 # start waybar again
 waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
