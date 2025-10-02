d142688c417625f3c38f1745f26c85244a8803e4
jpwol <jpwolfe1981@gmail.com>
2025-10-02 19:34:17 -0400

    nvim-dap-python is fucking broken

--- .config/nvim/lua/plugins/nvimdap.lua
+++ .config/nvim/lua/plugins/nvimdap.lua
@@ -54,16 +54,16 @@ return {
 			end
 		end,
 	},
-	{
-		"mfussenegger/nvim-dap-python",
-		ft = "python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function()
-			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-			require("dap-python").setup(path)
-		end,
-	},
+	-- {
+	-- 	"mfussenegger/nvim-dap-python",
+	-- 	ft = "python",
+	-- 	dependencies = {
+	-- 		"mfussenegger/nvim-dap",
+	-- 		"rcarriga/nvim-dap-ui",
+	-- 	},
+	-- 	config = function()
+	-- 		local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+	-- 		require("dap-python").setup(path)
+	-- 	end,
+	-- },
 }
