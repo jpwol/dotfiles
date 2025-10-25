4d7035c643deda0dad035c51ad33638acebf3827
jpwol <jpwolfe1981@gmail.com>
2025-10-24 21:39:32 -0400

    fix(hl-colors): certain buffers will not be affected

--- .config/nvim/lua/plugins/hl-colors.lua
+++ .config/nvim/lua/plugins/hl-colors.lua
@@ -1,6 +1,12 @@
 return {
 	"brenoprata10/nvim-highlight-colors",
 	config = function()
-		require("nvim-highlight-colors").setup({})
+		require("nvim-highlight-colors").setup({
+			render = "virtual",
+			virtual_symbol = "■",
+
+			exclude_filetypes = { "lazy" },
+			exclude_buftypes = { "nofile" },
+		})
 	end,
 }
