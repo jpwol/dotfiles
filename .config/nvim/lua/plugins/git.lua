6957bfc7955655be2fcc0bb9d91c214d3171b7b5
jpwol <jpwolfe1981@gmail.com>
2025-10-25 01:49:59 -0400

    refactor(git): removed conflicting keybind

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -8,30 +8,10 @@ return {
 		priority = 1000,
 		config = function()
 			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gg", "<cmd>Gitsigns<CR>", { desc = "Open Git menu" })
+			-- vim.keymap.set("n", "<leader>gg", "<cmd>Gitsigns<CR>", { desc = "Open Git menu" })
 			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Open the preview menu" })
 			vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<CR>", { desc = "View next git hunk" })
 			vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<CR>", { desc = "View previous git hunk" })
 		end,
 	},
-	-- {
-	-- 	"kdheepak/lazygit.nvim",
-	-- 	lazy = false,
-	-- 	cmd = {
-	-- 		"LazyGit",
-	-- 		"LazyGitConfig",
-	-- 		"LazyGitCurrentFile",
-	-- 		"LazyGitFilter",
-	-- 		"LazyGitFilterCurrentFile",
-	-- 	},
-	-- 	-- optional for floating window border decoration
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 	},
-	-- 	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- 	-- order to load the plugin when the command is run for the first time
-	-- 	keys = {
-	-- 		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-	-- 	},
-	-- },
 }
