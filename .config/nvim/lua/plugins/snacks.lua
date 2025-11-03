1bd0ea6346a52da845ef2075d55f8fae11f0a803
jpwol <jpwolfe1981@gmail.com>
2025-11-03 11:35:52 -0500

    feat: lsp goto now opens in a split

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -47,8 +47,8 @@ return {
 		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Find Files", },
 
     -- Lsp
-    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    { "gd", ":vsplit | lua Snacks.picker.lsp_definitions()<CR>" , desc = "Goto Definition" },
+    { "gD", ":vsplit | lua Snacks.picker.lsp_declarations()<CR>", desc = "Goto Declaration" },
     { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
