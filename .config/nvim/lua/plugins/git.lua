88fa0022988cda4879763fe8c954939324c365f8
jpwol <jpwolfe1981@gmail.com>
2025-11-03 11:35:30 -0500

    feat: added keymap for Gitsigns

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -8,7 +8,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("gitsigns").setup()
-			-- vim.keymap.set("n", "<leader>gg", "<cmd>Gitsigns<CR>", { desc = "Open Git menu" })
+			vim.keymap.set("n", "<leader>gs", "<cmd>Gitsigns<CR>", { desc = "Open Git menu" })
 			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Open the preview menu" })
 			vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<CR>", { desc = "View next git hunk" })
 			vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<CR>", { desc = "View previous git hunk" })
