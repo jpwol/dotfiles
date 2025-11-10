2c380a2e1dd32abb96b6075aef0c452289ed68b5
jpwol <jpwolfe1981@gmail.com>
2025-11-09 20:26:34 -0500

    refactor(colorscheme): removed useless config options for tokyonight colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,30 +2,14 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
-		opts = {
-			style = "moon",
-			styles = {
-				keywords = { italic = true },
-				sidebars = "transparent",
-				-- floats = "transparent",
-			},
-			on_highlights = function(hl, c)
-				hl.DiagnosticUnderlineWarn.undercurl = false
-				hl.DiagnosticUnderlineWarn.underline = true
-				hl.DiagnosticUnderlineError.undercurl = false
-				hl.DiagnosticUnderlineError.underline = true
-				hl.DiagnosticUnderlineHint.undercurl = false
-				hl.DiagnosticUnderlineHint.underline = true
-				hl.DiagnosticUnderlineInfo.undercurl = false
-				hl.DiagnosticUnderlineInfo.underline = true
-			end,
-		},
 	},
 	{
 		"jpwol/thorn.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		opts = {
+			transparent = true,
+		},
 	},
 	{
 		"neanias/everforest-nvim",
