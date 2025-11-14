c689ef135a3ef4a4164d7605645842efcbef6721
jpwol <jpwolfe1981@gmail.com>
2025-11-14 14:41:29 -0500

    fix: updated deprecated sign-define function

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -121,12 +121,17 @@ return {
 			local capabilities = cmp_nvim_lsp.default_capabilities()
 
 			-- Change the Diagnostic symbols in the sign column (gutter)
-			-- (not in youtube nvim video)
-			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
+			local signs = {
+				[vim.diagnostic.severity.ERROR] = " ",
+				[vim.diagnostic.severity.WARN] = " ",
+				[vim.diagnostic.severity.HINT] = "󰠠 ",
+				[vim.diagnostic.severity.INFO] = " ",
+			}
+			vim.diagnostic.config({
+				signs = {
+					text = signs,
+				},
+			})
 
 			vim.lsp.config("sqls", {
 				capabilities = capabilities,
